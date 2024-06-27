62524b04d8157cd0386e0f7366b37bbc0e13c1d9
scrawach <scrawach@gmail.com>
2024-06-27 15:31:30 +0300

    added empty BootstrapInstaller, prepared Bootstrap scene with scene context and attached installer on project context

---
+++ src/KeyboardMages/Assets/CodeBase/Infrastructure/Installers/BootstrapInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace CodeBase.Infrastructure.Installers
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
