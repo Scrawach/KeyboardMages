2172007b674ef94de4e13407e7e7b8b756b44d74
scrawach <scrawach@gmail.com>
2024-07-03 10:29:41 +0300

    added link to core assembly in tests, added empty test for system factory

---
+++ src/KeyboardMages/Assets/Tests/EditMode/Infrastructure/Systems/SystemFactoryTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Tests.EditMode.Infrastructure.Systems
+{
+    public class SystemFactoryTests
+    {
+        [Test]
+        public void WhenCreateSystem_ThenShouldReturnSystem()
+        {
+            // arrange
+            
+            // act
+            
+            // assert
+        }
+    }
+}
