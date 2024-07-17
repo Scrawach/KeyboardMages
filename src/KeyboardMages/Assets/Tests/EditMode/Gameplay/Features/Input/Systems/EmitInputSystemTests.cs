3c22b6af2660a3ee0d37053797428d3b2c8c1f77
scrawach <scrawach@gmail.com>
2024-07-17 11:05:37 +0300

    added test for emit input system about remove input axis component without input

--- src/KeyboardMages/Assets/Tests/EditMode/Gameplay/Features/Input/Systems/EmitInputSystemTests.cs
+++ src/KeyboardMages/Assets/Tests/EditMode/Gameplay/Features/Input/Systems/EmitInputSystemTests.cs
@@ -33,6 +33,31 @@ namespace Tests.EditMode.Gameplay.Features.Input.Systems
             entity.hasAxisInput.Should().BeTrue();
             entity.AxisInput.Should().Be(axis);
         }
+
+        [Test]
+        public void WhenExecute_AndHasNotInput_ThenShouldRemoveInputAxisComponent_OnInputEntity_WithInputComponent()
+        {
+            // arrange
+            var axis = new Vector2(25f, 25f);
+            var mockedInput = new MockedInput(true, axis);
+            var contexts = new Contexts();
+            
+            var emitInputSystem = new EmitInputSystem(contexts.input, mockedInput);
+            contexts.input.CreateEntity().isInput = true;
+
+            // act
+            emitInputSystem.Execute();
+            mockedInput.HasAxis = false;
+            emitInputSystem.Execute();
+
+            // assert
+            var entities = contexts.input.GetEntities();
+            var entity = entities.Single();
+            
+            entities.Should().NotBeEmpty();
+            entity.isInput.Should().BeTrue();
+            entity.hasAxisInput.Should().BeFalse();
+        }
         
         public class MockedInput : IInput
         {
