4742ef932f2cc7b6e1f742ab29524d3e8e1cc17b
scrawach <scrawach@gmail.com>
2024-07-23 11:20:16 +0300

    added gameplay feature with input feature

---
+++ src/KeyboardMages/Assets/CodeBase/Gameplay/GameplayFeature.cs
@@ -0,0 +1,13 @@
+using CodeBase.Gameplay.Features.Input;
+using CodeBase.Infrastructure.Systems;
+
+namespace CodeBase.Gameplay
+{
+    public class GameplayFeature : GenericFeature
+    {
+        public GameplayFeature(ISystemFactory systems) : base(systems)
+        {
+            Add<InputFeature>();
+        }
+    }
+}
